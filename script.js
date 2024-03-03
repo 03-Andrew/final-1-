0ffeac05925d05f4df4bda2c5e48770270a90fe1
03-Andrew <andreinadela052@gmail.com>
2024-03-03 22:23:08 +0800

    DOne

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ function addToCart() {
   deleteButton.className = "add_to_cart_btn";
   deleteButton.onclick = function () {
     cartItems.removeChild(item);
-      };
+  };
 
   item.appendChild(deleteButton);
   cartItems.appendChild(item);
@@ -136,4 +136,44 @@ function clearCart() {
   cartItems.innerHTML = "";
 }
 
+function sendBookingEmail() {
+  var name = document.getElementById('name').value;
+  var contactNumber = document.getElementById('contactNumber').value;
+  var email = document.getElementById('email').value;
+  var branch = document.getElementById('branch').value;
+  var dateTime = document.getElementById('datePicker').value;
+  var services = Array.from(cartItems.childNodes)
+    .map(node => node.textContent.replace('X', ''))
+    .filter(text => text.trim() !== '')
+    .join('\n');
+  var params = {
+    name: name,
+    contactNumber: contactNumber,
+    email: email,
+    branch: branch,
+    dateTime: dateTime,
+    services: services
+  }
+  const serviceID = "service_ofgus2j";
+  const templateID = "template_fuwug4j";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(function (response) {
+      alert("Booking Sent, Just wait for an reply to confirm your Booking");
+      console.log("Email sent successfully", response);
+      clearForm();
+    })
+    .catch(function (error) {
+      console.error("Email sending failed", error);
+    });
+}
+
+function clearForm() {
+  document.getElementById('name').value = '';
+  document.getElementById('contactNumber').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('branch').value = '';
+  document.getElementById('datePicker').value = '';
+  document.getElementById('cartItems').innerHTML = '';
+}
 
